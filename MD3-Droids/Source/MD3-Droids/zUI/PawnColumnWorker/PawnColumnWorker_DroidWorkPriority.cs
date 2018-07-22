d06520571725ff93b5f2bbd7a5f95ae60d100bbd
Liam <mipen@windowslive.com>
2018-07-22 21:47:31 +1200

    Finished work tab

---
+++ MD3-Droids/Source/MD3-Droids/zUI/PawnColumnWorker/PawnColumnWorker_DroidWorkPriority.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace MD3_Droids
+{
+    public class PawnColumnWorker_DroidWorkPriority : PawnColumnWorker_WorkPriority
+    {
+        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
+        {
+            if (pawn is Droid)
+            {
+                Droid droid = pawn as Droid;
+                if (!pawn.Dead && pawn.workSettings != null && pawn.workSettings.EverWork)
+                {
+
+                    Text.Font = GameFont.Medium;
+                    float x = rect.x + (rect.width - 25f) / 2f;
+                    float y = rect.y + 2.5f;
+                    bool incapable = false;// IsIncapableOfWholeWorkType(pawn, def.workType);
+                    if (droid.aiPackages.CapableOfWorkType(def.workType))
+                        WidgetsWork.DrawWorkBoxFor(x, y, pawn, def.workType, incapable);
+                    Rect rect2 = new Rect(x, y, 25f, 25f);
+                    TooltipHandler.TipRegion(rect2, () => WidgetsWork.TipForPawnWorker(pawn, def.workType, incapable), pawn.thingIDNumber ^ def.workType.GetHashCode());
+                    Text.Font = GameFont.Small;
+                }
+            }
+        }
+    }
+}
